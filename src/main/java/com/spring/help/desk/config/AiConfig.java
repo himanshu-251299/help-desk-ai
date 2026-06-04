e935ea6287a9c60382e230740e66f898cef8284a
himanshu_45 <himanshu.yadav@cyberevolve.com>
2026-06-04 12:15:33 +0530

    Replace InMemory chat memory to Jdbc chat memory

--- src/main/java/com/spring/help/desk/config/AiConfig.java
+++ src/main/java/com/spring/help/desk/config/AiConfig.java
@@ -3,7 +3,8 @@ package com.spring.help.desk.config;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,13 @@ import org.springframework.context.annotation.Configuration;
 public class AiConfig {
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory){
+    public ChatClient chatClient(ChatClient.Builder builder, JdbcChatMemoryRepository jdbcChatMemoryRepository){
+
+        var chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(20)
+                .build();
+
         return builder.defaultAdvisors(new SimpleLoggerAdvisor(),
                 MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
